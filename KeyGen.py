eabc73f686fc56ff4965bdb429b18b85cdd625c6
kingmak <kingmak@users.noreply.github.com>
2017-08-02 21:38:06 -0500

    Update KeyGen.py

--- KeyGen.py
+++ KeyGen.py
@@ -3,10 +3,3 @@ import string, itertools
 charSet = string.printable[:36] # [0-9a-z]
 for key in itertools.product(charSet, repeat = 5): # ghostbin ids are length 5
     print key
-
-'''
-# grep  "'n', 'z', 'n', '8', 't'" keys.txt
-#for subset in itertools.combinations(stuff, 5):
-#for subset in itertools.permutations(stuff, 5):
-#    print(subset) 
-'''     
